cabd575e37e38501d8d66cfa4140d3ad1a99f7d5
SamirM-dev <memmedovsamirws@gmail.com>
2026-07-21 17:05:33 +0400

    Interceptors

---
+++ src/main/java/com/example/taskmanager/config/FilterConfig.java
@@ -0,0 +1,40 @@
+package com.example.taskmanager.config;
+
+import com.example.taskmanager.filter.AuthFilter;
+import com.example.taskmanager.filter.LogFilter;
+import com.example.taskmanager.filter.RequestIdFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<RequestIdFilter> requestIdFilter(){
+        FilterRegistrationBean<RequestIdFilter> bean= new FilterRegistrationBean<>();
+        bean.setFilter(new RequestIdFilter());
+        bean.setOrder(1);
+        bean.addUrlPatterns("/api/**");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<LogFilter> logFilter(){
+        FilterRegistrationBean<LogFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new LogFilter());
+        bean.setOrder(2);
+        bean.addUrlPatterns("/api/**");
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthFilter> authFilter(){
+        FilterRegistrationBean<AuthFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new AuthFilter());
+        bean.setOrder(3);
+        bean.addUrlPatterns("/api/**");
+        return bean;
+    }
+
+
+}
---
+++ src/main/java/com/example/taskmanager/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.taskmanager.config;
+
+import com.example.taskmanager.interceptor.LogInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final LogInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/api/**").excludePathPatterns("/api/v1/auth/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedHeaders("Authorization,Content-Type").exposedHeaders("Location,X-Request-ID").allowCredentials(true).allowedOrigins("http://localhost:3000").allowedMethods("GET,POST,UPDATE,PATCH,DELETE");
+    }
+}
